932663be126921a8877c4e884db7acf246526fe6
Bruna Mayumi Hori <brunahori19@gmail.com>
2023-10-16 15:15:39 -0300

    Add files via upload

---
+++ live_detect.py
@@ -0,0 +1,33 @@
+# importe a biblioteca opencv 
+import cv2
+
+# Defina um objeto VideoCapture
+vid = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+while(True):
+   
+    # Capture o vídeo quadro a quadro
+    ret, frame = vid.read()
+
+    #CONVERTENDO A IMAGEM DE ENTRADA EM CINZA
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #detecta o rosto
+    rosto = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for(x,y,w,h) in rosto:
+        cv2.rectangle(frame,(x,y), (x+w, y+h), (0,255,0),3)
+
+
+    # Exiba o quadro resultante
+    cv2.imshow("Web cam", frame)
+      
+    # Saia da tela ao pressionar a barra de espaço
+    if cv2.waitKey(25) == 32:
+        break
+  
+# Após o loop, libere o objeto capturado
+vid.release()
+
+# Destrua todas as telas
+cv2.destroyAllWindows()
